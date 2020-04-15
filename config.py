d64a79bd91e00a793d5f26e9698591f7377ae15a
Gord <gordpwest@gmail.com>
2020-04-15 14:09:33 -0600

    Add files

---
+++ config.py
@@ -0,0 +1,9 @@
+
+# API config
+URL = 'https://www.alphavantage.co/query'
+API_KEY = 'SHAJ7XTL7NCWJXNP'
+
+# Discord bot config
+
+
+# SQLconfig
---
+++ stonks.py
@@ -0,0 +1,48 @@
+import requests
+import json
+from config import API_KEY, URL
+
+# returns top 5 stock symbols from search keyword
+def top5Search(keywords):
+    params = {
+        'function': 'SYMBOL_SEARCH',
+        'keywords': keywords,
+        'apikey': API_KEY
+    }
+    response = requests.request('GET', URL, params=params)
+    top5 = response.json()['bestMatches'][:5]
+    return top5
+
+# return first canadian symbol otherwise return overall top result
+def symbolSearch(keywords):
+    params = {
+        'function': 'SYMBOL_SEARCH',
+        'keywords': keywords,
+        'apikey': API_KEY
+    }
+    response = requests.request('GET', URL, params=params)
+    results = response.json()['bestMatches']
+    for stock in results:
+        if (stock['4. region'] == 'Toronto'):
+            return stock['1. symbol']
+            break
+    return results[0]['1. symbol']
+
+# return stock data for a given symbol
+def stockData(symbol):
+    params = {
+        'function': 'GLOBAL_QUOTE',
+        'symbol': symbol,
+        'apikey': 'SHAJ7XTL7NCWJXNP'
+    }
+    response = requests.request('GET', URL, params=params)
+    results = response.json()
+    return results
+
+
+STONK = 'Shopify'
+
+json_formatted_str = json.dumps(stockData(symbolSearch(STONK)), indent=2)
+print(json_formatted_str)
+
+
